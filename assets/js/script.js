66c2e58f4e66c9ec7b14609c5676ea7d4e9edd26
EmyDvger <emilieduverger_80@yahoo.com>
2019-11-27 14:40:26 +0100

    exo javascript 5

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+
+    //fonction validation "onclick", je veux obtenir ceux ci
+    function validation() {
+      //declaration des variables
+      var numberA = document.getElementById('numberA').value;
+      var numberB = document.getElementById('numberB').value;
+      /*declaration des variables result qui
+      contient le résultat de la division du nombreA par le nombreB*/
+      var result = numberA / numberB
+      alert(result);
+    }
